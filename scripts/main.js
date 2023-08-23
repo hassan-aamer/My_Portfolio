81278d4127acac80b07dc435e48dd8101de205b3
Hassan ツ <130797396+hassan-aamer@users.noreply.github.com>
2023-08-23 15:08:08 +0300

    New Update

--- scripts/main.js
+++ scripts/main.js
@@ -74,3 +74,12 @@ let scrollTop = document.querySelector(".scroll-top")
 window.addEventListener("scroll", () => {
     scrollTop.classList.toggle("scroll-active", window.scrollY > 400);
 });
+
+// Custom Scroll Bar
+window.onscroll = function() {mufunction()};
+function mufunction(){
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById('scroll-bar').style.width = scrolled + '%';
+}
