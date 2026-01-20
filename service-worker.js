4177cd2d28b355f48b1ed448b6e4a62c35279a90
hassan-aamer <hassanaamer048@gmail.com>
2026-01-20 12:02:37 +0200

    chore: bump service worker cache version to v8

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * Enables offline functionality and caching
  */
 
-const CACHE_NAME = 'hassan-portfolio-v7';
+const CACHE_NAME = 'hassan-portfolio-v8';
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
