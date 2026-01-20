970b3795e8f3951dfc6972422387756c762b7565
hassan-aamer <hassanaamer048@gmail.com>
2026-01-20 12:14:01 +0200

    chore: Update service worker cache version from v8 to v9.

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * Enables offline functionality and caching
  */
 
-const CACHE_NAME = 'hassan-portfolio-v8';
+const CACHE_NAME = 'hassan-portfolio-v9';
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
