65e1be2d41e1dbe3e9a764278d351a49c12f1a1e
hassan-aamer <hassanaamer048@gmail.com>
2026-06-23 04:09:49 +0300

    u

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * Enables offline functionality and caching
  */
 
-const CACHE_NAME = 'hassan-portfolio-v17';
+const CACHE_NAME = 'hassan-portfolio-v18';
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
